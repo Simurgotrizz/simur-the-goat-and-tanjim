1c98a7e4424996461df0a5b04a60c22a2e88170a
Simurgotrizz <136985568+Simurgotrizz@users.noreply.github.com>
2023-08-30 19:33:09 -0400

    Update simur.py

--- simur.py
+++ simur.py
@@ -1,15 +1,8 @@
 import streamlit as st
 
-def main():
-    st.title("Plant Cell vs. Refrigerator Comparison")
-    st.write("Explore the similarities and differences between a plant cell and a refrigerator!")
-
-    # Load images
-    plant_cell_image = "plant_cell.jpg"
-    refrigerator_image = "refridgerator.jpg"
-
-    st.image(plant_cell_image, caption="Plant Cell", use_column_width=True)
-    st.image(refrigerator_image, caption="Refrigerator", use_column_width=True)
+# Page Title and Header
+st.title("Comparing a Refrigerator to Parts of a Plant Cell")
+st.header("An Analogous Comparison")
 
 # Introduction
 st.markdown(
@@ -19,10 +12,10 @@ st.markdown(
 )
 
 # Plant Cell Image
-st.image("plant_cell.png", caption="Plant Cell", use_column_width=True)
+st.image("plant_cell.jpg", caption="Plant Cell", use_column_width=True)
 
 # Refrigerator Image
-st.image("refrigerator.png", caption="Refrigerator", use_column_width=True)
+st.image("refridgerator.jpg", caption="Refrigerator", use_column_width=True)
 
 # Comparison Table
 st.header("Comparison Table")
@@ -53,5 +46,3 @@ st.markdown(
     "it's interesting to draw parallels between their components. Both examples highlight "
     "how individual parts contribute to the overall functioning of the system."
 )
-if __name__ == "__main__":
-    main()
