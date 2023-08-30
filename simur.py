83dfad1b952080c6ae2341ce0acc82c05b003271
Simurgotrizz <136985568+Simurgotrizz@users.noreply.github.com>
2023-08-30 19:26:07 -0400

    Update simur.py

--- simur.py
+++ simur.py
@@ -11,21 +11,52 @@ def main():
     st.image(plant_cell_image, caption="Plant Cell", use_column_width=True)
     st.image(refrigerator_image, caption="Refrigerator", use_column_width=True)
 
-    st.header("Comparison:")
-    st.subheader("1. Structure")
-    st.write("A plant cell has a defined structure with various organelles, similar to the compartments and shelves in a refrigerator.")
 
-    st.subheader("2. Energy Source")
-    st.write("Plant cells generate energy through photosynthesis using chloroplasts. A refrigerator requires electricity to power its cooling system.")
+# Page Title and Header
+st.title("Comparing a Refrigerator to Parts of a Plant Cell")
+st.header("An Analogous Comparison")
 
-    st.subheader("3. Storage")
-    st.write("Both entities have storage capabilities: a plant cell stores nutrients and water, while a refrigerator stores food and beverages.")
+# Introduction
+st.markdown(
+    "Just as different parts of a plant cell work together to ensure its proper functioning, "
+    "a refrigerator also consists of various components that contribute to its operation. "
+    "Let's explore the similarities between them!"
+)
 
-    st.subheader("4. Regulation")
-    st.write("Plant cells regulate water and nutrients using the cell membrane and vacuole, while a refrigerator regulates temperature and humidity.")
+# Plant Cell Image
+st.image("plant_cell.png", caption="Plant Cell", use_column_width=True)
 
-    st.subheader("5. Maintenance")
-    st.write("Regular maintenance of a refrigerator ensures efficient cooling, just as a plant cell requires proper care for its health.")
+# Refrigerator Image
+st.image("refrigerator.png", caption="Refrigerator", use_column_width=True)
 
+# Comparison Table
+st.header("Comparison Table")
+
+comparison_data = {
+    "Plant Cell": [
+        "Cell Membrane",
+        "Nucleus",
+        "Mitochondria",
+        "Endoplasmic Reticulum",
+        "Golgi Apparatus",
+    ],
+    "Refrigerator": [
+        "Door Seal",
+        "Thermostat",
+        "Compressor",
+        "Cooling Coils",
+        "Fan",
+    ],
+}
+
+st.table(comparison_data)
+
+# Conclusion
+st.header("Conclusion")
+st.markdown(
+    "While a plant cell and a refrigerator serve completely different purposes, "
+    "it's interesting to draw parallels between their components. Both examples highlight "
+    "how individual parts contribute to the overall functioning of the system."
+)
 if __name__ == "__main__":
     main()
